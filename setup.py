928e110dc95462467b0e626d6997461e0f5f4c5a
Jake VanCampen <jake.vancampen7@gmail.com>
2019-07-19 20:00:21 -0700

    add environment.yml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from distutils.core import setup
 from setuptools import find_packages
 
-__version__ = "1.0.0"
+__version__ = "0.0.1"
 
 install_requires = [
     "matplotlib", "pandas", "numpy",
